139b0e2a72b8b2fde890e57a7a7bea5b80828092
ShrutiBelsare27 <shrutibelsare27@gmail.com>
2021-10-01 23:35:19 +0530

    Added: To find day and month

---
+++ SelectionIfElse/DayAndMonth.sh
@@ -0,0 +1,47 @@
+x
+
+#!/bin/bash 
+#read -p "enter a day " day
+#read -p "enter a month" month
+echo "enter a day";
+read day
+echo "enter a month ";
+read month
+mar=3
+apr=4
+may=5
+june=6
+if [ $mar -eq $month ] 
+then 
+     if [ $day -ge 20 ] && [ $day -le  31 ]
+     then
+          echo "true"
+     else
+          echo "false" 
+     fi
+elif [ $june -eq $month ]
+then
+     if [ $day -ge 1 ] && [ $day -le 20 ]
+     then
+          echo "true"
+     else
+
+          echo "false" 
+     fi
+elif [ $apr -eq $month ]
+then
+     if [ $day -ge 1 ] && [ $day -le  30 ]
+     then
+          echo "true"
+     else
+          echo "false"
+     fi
+elif [ $may -eq $month ]
+then
+     if [ $day -ge 1 ] && [ $day -le  31 ]
+     then
+          echo "true"
+     else
+          echo "false"
+fi
+fi
